a88ab4ef2d572a9d0732dbc67cb97143f95719b8
suzannejin <suzanne.jin@crg.eu>
2020-02-05 12:28:39 +0100

    Data manipulation scripts

---
+++ bin/organize_data/delta_data.sh
@@ -0,0 +1,55 @@
+
+
+declare -a aligner=(CLUSTALO MAFFT-FFTNS1 MAFFT-GINSI)
+
+declare -f folders=(results_fullTree_CO results_fullTree_FFTNS1 results_fullTree_GINSI)
+
+declare -a familyName=(ltn     il8     az      kringle cryst   DEATH   cah     mmp     rub     ghf10   tgfb    sodcu   KAS     DMRL_synthase   tms     GEL     kunitz  Sulfotransfer   mofe    Ald_Xan_dh_2    ghf5    phc     aadh    annexin serpin  cytb    asp     oxidored_q6     hpr     hormone_rec     hr      tim     glob    ace     cys     ghf1    sodfe   peroxidase      uce     flav    HMG_box OTCace  msb     icd     proteasome      cyclo   LIM     HLH     ldh     subt    int     lyase_1 gpdh    egf     blm     gluts   myb_DNA-binding tRNA-synt_2b    biotin_lipoyl   hom     ghf13   aldosered       hla     Rhodanese       PDZ     blmb    rhv     p450    adh     aat     rrm     Acetyltransf    sdr     zf-CCHH rvp)
+##declare -a familyName=(seatoxin)
+
+declare -a tree=(codnd dpparttreednd1 dpparttreednd2 dpparttreednd2size fastaparttreednd fftns1dnd fftns1dndmem fftns2dnd fftns2dndmem mafftdnd parttreednd0 parttreednd1 parttreednd2 parttreednd2size)
+##declare -a tree=(codnd dpparttreednd1 dpparttreednd2)
+
+declare -a tcs=(tc sp col)
+declare -a homos=(homo w_homo w_homo2 len ngap ngap2)
+
+
+printf "family aligner tree1 tree2 tc sp col homo w_homo w_homo2 len ngap ngap2\n"
+
+for fam in ${familyName[@]}; do
+    i=0
+    for aln in ${aligner[@]}; do
+        fold=${folders[$i]}
+        ntr=0
+        for tr1 in ${tree[@]}; do
+        ntr=$((ntr+1))
+        for tr2 in ${tree[@]:$ntr}; do
+            printf "$fam $aln $tr1 $tr2 "
+            for tc in ${tcs[@]}; do
+                fil1=${fold}/individual_scores/${fam}.dpa_align.1000.${aln}.${tr1}.${tc}
+                fil2=${fold}/individual_scores/${fam}.dpa_align.1000.${aln}.${tr2}.${tc}
+                if [[ -s $fil1 && -s $fil2 ]]; then
+                    val1=$(awk 'NR==1{printf $1" "}' ${fil1})
+                    val2=$(awk 'NR==1{printf $1" "}' ${fil2})
+                    python -c "print $val1 - $val2" | tr "\n" " "
+                else
+                    printf "NA "
+                fi
+            done
+            for hom in ${homos[@]}; do
+                fil1=${fold}/alignments/${fam}.dpa_1000.${aln}.with.${tr1}.tree.${hom}
+                fil2=${fold}/alignments/${fam}.dpa_1000.${aln}.with.${tr2}.tree.${hom}
+                if [[ -s $fil1 && -s $fil2 ]]; then
+                    val1=$(awk 'NR==1{printf $1" "}' ${fil1} | tr -d "l")
+                    val2=$(awk 'NR==1{printf $1" "}' ${fil2} | tr -d "l")
+                    python -c "print $val1 - $val2" | tr "\n" " "
+                else
+                    printf "NA "
+                fi
+            done
+            printf "\n"
+        done
+        done
+        i=$((i+1))
+    done
+done
---
+++ bin/organize_data/merge_data.sh
@@ -0,0 +1,50 @@
+
+
+declare -a aligner=(CLUSTALO MAFFT-FFTNS1 MAFFT-GINSI)
+
+declare -f folders=(results_fullTree_CO results_fullTree_FFTNS1 results_fullTree_GINSI)
+
+declare -a familyName=(ltn     il8     az      kringle cryst   DEATH   cah     mmp     rub     ghf10   tgfb    sodcu   KAS     DMRL_synthase   tms     GEL     kunitz  Sulfotransfer   mofe    Ald_Xan_dh_2    ghf5    phc     aadh    annexin serpin  cytb    asp     oxidored_q6     hpr     hormone_rec     hr      tim     glob    ace     cys     ghf1    sodfe   peroxidase      uce     flav    HMG_box OTCace  msb     icd     proteasome      cyclo   LIM     HLH     ldh     subt    int     lyase_1 gpdh    egf     blm     gluts   myb_DNA-binding tRNA-synt_2b    biotin_lipoyl   hom     ghf13   aldosered       hla     Rhodanese       PDZ     blmb    rhv     p450    adh     aat     rrm     Acetyltransf    sdr     zf-CCHH rvp)
+##declare -a familyName=(seatoxin)
+
+declare -a tree=(codnd dpparttreednd1 dpparttreednd2 dpparttreednd2size fastaparttreednd fftns1dnd fftns1dndmem fftns2dnd fftns2dndmem mafftdnd parttreednd0 parttreednd1 parttreednd2 parttreednd2size)
+##declare -a tree=(codnd dpparttreednd1 dpparttreednd2)
+
+declare -a tcs=(tc sp col)
+declare -a homos=(homo w_homo w_homo2 len ngap ngap2)
+
+
+printf "family aligner tree tc sp col homo w_homo w_homo2 len ngap ngap2\n"
+
+for fam in ${familyName[@]}; do
+    i=0
+    for aln in ${aligner[@]}; do
+        fold=${folders[$i]}
+        for tr in ${tree[@]}; do
+            printf "$fam $aln $tr "
+            for tc in ${tcs[@]}; do
+                fil=${fold}/individual_scores/${fam}.dpa_align.1000.${aln}.${tr}.${tc}
+                if [[ -s $fil ]]; then
+                    awk 'NR==1{printf $1" "}' ${fil}
+                else
+                    printf "NA "
+                fi
+            done
+            for hom in ${homos[@]}; do
+                fil=${fold}/alignments/${fam}.dpa_1000.${aln}.with.${tr}.tree.${hom}
+                if [[ -s $fil ]]; then
+                    if [[ $hom == "ngap2" ]]; then 
+                        awk 'NR==1{printf $1" "}' ${fil} | tr -d "l"
+                    else
+                        awk 'NR==1{printf $1" "}' ${fil}
+                    fi
+                else
+                    printf "NA "
+                fi
+            done
+            printf "\n"
+        done
+        i=$((i+1))
+    done
+done
+
