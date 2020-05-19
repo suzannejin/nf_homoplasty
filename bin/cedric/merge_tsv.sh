2fb5af9dd9f38cfac3b3e81dffa80dbcbb6f0cfe
suzannejin <suzanne.jin@crg.eu>
2020-05-19 13:07:53 +0200

    Deleted unnecessary scripts

--- bin/cedric/merge_tsv.sh
+++
@@ -1,8 +0,0 @@
-folder=$1
-typ=$2  # fam, aln, global
-out=${folder}/out.${typ}.tsv
-
-cat ${folder}/*.${typ}.tsv | head -n 1 > $out
-for fil in $(echo ${folder}/*.${typ}.tsv); do
-    awk 'NR!=1{print}' $fil >> $out
-done
